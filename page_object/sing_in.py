3789d6891a2d994098ee7ba275d0c575c439686e
Ierima Ruxandra <ruxi.ierima@gmail.com>
2018-11-15 20:56:26 +0200

    Add some pages

---
+++ page_object/sing_in.py
@@ -0,0 +1,43 @@
+
+class SingIn():
+
+    #Constructor
+    def __init__(self):
+        pass
+
+    #Class variables and locators
+
+    #Clicks on 'Email Address' field
+    def click_email_address(self):
+        pass
+
+    # Clicks on 'Password' field
+    def click_password(self):
+        pass
+
+    # Clicks on 'Lost your password?' button
+    def click_lost_your_password_button(self):
+        return self.LostYourPassword()
+
+
+    class LostYourPassword():
+
+        # Class variables and locators
+
+        # Clicks on 'Email Address' field
+        def click_email_address(self):
+            pass
+
+        def click_reset_password_button(self):
+            pass
+
+        def click_cancel_button(self):
+            pass
+
+    class CreateAccount():
+        pass
+
+
+
+
+
